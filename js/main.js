9bf73caac961a69569b2c7d6b520c1c5bf94738b
sagor9116 <sagor.roy9116@gmail.com>
2021-09-07 08:28:40 +0600

    everything is complete

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-//navigation bar effect on scroll
-window.addEventListener('scroll',function(){
-    let headerScroll = this.document.querySelector('header');
-    headerScroll.classList.toggle('sticky', window.scrollY > 0);
-})
 
-//navigation bar toggle 
-const navbarToggle = document.querySelector('.navbar__toggle');
-const navigation = document.getElementById('navigation');
-navbarToggle.addEventListener('click',function() {
-    navigation.classList.toggle('active')
-})
+
+
+
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      navbar.classList.add("show");
+      menuBtn.classList.add("hide");
+      
+    }
+    cancelBtn.onclick = ()=>{
+      
+      navbar.classList.remove("show");
+      menuBtn.classList.remove("hide");
+    }
+    
+    
+   window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
